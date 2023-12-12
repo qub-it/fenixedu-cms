91ed3c332739b99fe66d11996a540c1e29c844ba
João Freitas <joao.freitas@qub-it.com>
2023-12-12 16:26:22 +0000

    Registers missing SiteViewers group in tests

--- src/test/java/org/fenixedu/bennu/core/groups/ManualGroupRegister.java
+++ src/test/java/org/fenixedu/bennu/core/groups/ManualGroupRegister.java
@@ -3,6 +3,8 @@ package org.fenixedu.bennu.core.groups;
 import org.fenixedu.bennu.core.groups.CustomGroupRegistry.BooleanParser;
 import org.fenixedu.bennu.core.groups.CustomGroupRegistry.DateTimeParser;
 import org.fenixedu.bennu.core.groups.CustomGroupRegistry.StringParser;
+import org.fenixedu.cms.domain.SiteViewersGroup;
+import org.fenixedu.cms.domain.SiteViewersGroup.SiteArgumentParser;
 
 public class ManualGroupRegister {
     private static boolean done = false;
@@ -11,11 +13,12 @@ public class ManualGroupRegister {
         if (!done) {
             CustomGroupRegistry.registerCustomGroup(AnonymousGroup.class);
             CustomGroupRegistry.registerCustomGroup(AnyoneGroup.class);
+            CustomGroupRegistry.registerCustomGroup(SiteViewersGroup.class);
             CustomGroupRegistry.registerCustomGroup(LoggedGroup.class);
             CustomGroupRegistry.registerCustomGroup(NobodyGroup.class);
             CustomGroupRegistry.registerCustomGroup(UserGroup.class);
             CustomGroupRegistry.registerArgumentParser(UserGroup.UserArgumentParser.class);
-
+            CustomGroupRegistry.registerArgumentParser(SiteArgumentParser.class);
             CustomGroupRegistry.registerArgumentParser(BooleanParser.class);
             CustomGroupRegistry.registerArgumentParser(StringParser.class);
             CustomGroupRegistry.registerArgumentParser(DateTimeParser.class);
